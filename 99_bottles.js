e1da12808e96b126e2e7f56b8b5aa81c4fd0597a
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-23 22:50:38 +0200

    working with loop

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,12 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(num) {
+  for (let i = num; i > 2; i--) {
+    let message = `${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${i-1} bottles of beer on the wall.`
+    console.log(message)
+  }
+  
+  let end = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+  console.log(end)
+
 };
 
-bottleSong();
+bottleSong(num);
